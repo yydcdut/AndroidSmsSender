83e5bdf1a5639b78dcd0f764cc7fbf6e2b4dda8d
yydcdut <378040621@qq.com>
2017-11-12 14:00:48 +0800

    Initial commit

---
+++ app/src/main/java/com/yydcdut/sms/IgnoreNumberFragment.kt
@@ -0,0 +1,17 @@
+package com.yydcdut.sms
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by yuyidong on 2017/11/12.
+ */
+open class IgnoreNumberFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
